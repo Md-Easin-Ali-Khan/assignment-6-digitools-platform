695433db1a91c2cbf8acbc142556df1e84fb9e56
Md-Easin-Ali-Khane <mdeasinalikhan9@gmail.com>
2026-04-01 01:09:07 +0600

    fix the responsive issue of navigation bar

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -9,16 +9,16 @@ const NavBar = () => {
 
     const links = navData.map(link => (
         <li key={link.id}>
-            <a className="text-[16px] text-gray-900 font-semibold hover:text-purple-600" href={link.path}>
+            <a className="text-gray-900 font-semibold hover:text-purple-600" href={link.path}>
                 {link.name}
             </a>
         </li>
     ))
 
     return (
-        <div className="h-23 flex justify-between items-center relative">
+        <div className="md:h-24 flex flex-col justify-center md:flex-row md:justify-between items-center">
             <div className="my-6">
-                <div className="flex items-center">
+                <div className="flex items-center relative">
 
                     <button
                         className="md:hidden pr-3"
@@ -29,22 +29,28 @@ const NavBar = () => {
                         }
                     </button>
 
-                    <ul className={`md:hidden bg-white shadow-lg p-3 rounded transition-all duration-600 absolute space-y-3 ${open ? 'top-18' : '-top-96'}`}>
+                    <ul className={`md:hidden bg-white shadow-lg p-3 rounded transition-all duration-600 absolute space-y-3 ${open ? 'top-12' : '-top-96'}`}>
                         {links}
                     </ul>
 
                     <h1 className="text-4xl font-bold bg-gradient-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent">DigiTools</h1>
                 </div>
             </div>
-            <ul className="hidden md:flex gap-8 px-2">
+            <ul className="hidden md:flex space-x-4 px-2">
                 {
                     links
                 }
             </ul>
-            <div className="flex items-center gap-3">
-                <div><ShoppingCart className="w-4 text-gray-900 font-semibold"></ShoppingCart></div>
-                <button className="btn text-gray-900 font-semibold">Login</button>
-                <button className="btn bg-[linear-gradient(135deg,#4F39F6_0%,#9514FA_100%)] rounded-full text-white">Get Started</button>
+            <div className="flex items-center gap-4 md:gap-2 mb-6 md:mb-0">
+                <ShoppingCart className="w-4 block text-gray-900" />
+
+                <button className="btn text-gray-900 font-semibold">
+                    Login
+                </button>
+
+                <button className="btn bg-[linear-gradient(135deg,#4F39F6_0%,#9514FA_100%)] rounded-full text-white text-sm md:text-base">
+                    Get Started
+                </button>
             </div>
 
         </div>
